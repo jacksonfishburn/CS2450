6d5eb4e168a5b505a4c0ea128770896392594367
jacksonfishburn <jacksonfishburn@gmail.com>
2026-09-07 22:46:07 -0600

    fix method interface

--- classes/LoadStore.py
+++ classes/LoadStore.py
@@ -4,12 +4,12 @@ class LoadStore:
         self.registers = registers
         self.acc = acc
 
-    def load(i):
+    def load(self, i):
         # 20 load from memory into accumulator
         # acc = registers[i] 
         pass
 
-    def store(i):
+    def store(self, i):
         # 21 load from accumulator into memory
         # registers[i] = acc
         pass
